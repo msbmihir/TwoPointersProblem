4a85ba87983a3ab6d489ac6ea5f0380668212c17
Mihir Bhokre <58588532+msbmihir@users.noreply.github.com>
2021-01-04 12:23:01 +0530

    Add files via upload

---
+++ TwoPointers.java
@@ -0,0 +1,46 @@
+package luvBabbar;
+import java.util.Scanner;
+
+public class TwoPointers {
+	
+	static void shiftAll(int arr[], int left, int right) {
+		
+		while(left <= right) {
+			
+			if(arr[left]<0 && arr[right]<0) 
+				left++;
+			
+			else if(arr[left]>0 && arr[right]<0) {
+				int temp = arr[left];
+				arr[left] = arr[right];
+				arr[right] = temp;
+				left++;
+				right--;
+			}
+			else if(arr[left]>0 && arr[right]>0) {
+				right--;
+			}else {
+				left++;
+				right--;
+			}
+		}
+	}
+	static void display(int[] arr, int right)
+	{
+	     
+	    for(int i = 0; i <= right; i++)
+	        System.out.print(arr[i] + " ");
+	         
+	    System.out.println();
+	}
+	public static void main(String[] args) {
+		
+		int[] arr = { -12, 11, -13, -5, 6, -7, 5, -3, 11 };
+		      
+		int arr_size = arr.length;
+
+		shiftAll(arr, 0, arr_size - 1);
+		display(arr, arr_size - 1);
+		
+	}
+}
